6960dade656722b7c6b65cbc365ab61992b155e2
carlesalonso <carlos.martinez@mataro.epiaedu.cat>
2019-02-05 18:21:46 +0100

    First version using int

---
+++ tpldemo/Demos.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace tpldemo
+{
+
+    // Extension Class for int type that adds IsPrime method
+    public static class BaseTypesExtensions
+    {
+        public static bool IsPrime(this int n) //1 = false, 2 = true, 3 = true...
+        {
+            if (n <= 1) return false;
+            if ((n & 1) == 0)
+            {
+                if (n == 2) return true;
+                else return false;
+            }
+            for (int i = 3; (i * i) <= n; i += 2)
+            {
+                if ((n % i) == 0) return false;
+            }
+            return n != 1;
+        }
+    }
+
+
+
+    public class Demos
+    {
+        private const long N = 1000000;
+        private Stopwatch clock = new Stopwatch();
+        private int[] numbers = new int[N];
+       
+
+        public Demos()
+        {
+            // 
+            var rnd = new Random();
+            for (long i = 0; i < N; i++)
+               numbers[i] = rnd.Next(1, int.MaxValue);
+
+        }
+
+
+         public string SequentialFor(out double nPrimes)
+        {
+            nPrimes = 0;
+            clock.Restart();
+            for (long i = 0; i < N; i++)
+            {
+                if (numbers[i].IsPrime())
+                    nPrimes++;
+            }
+            clock.Stop();
+            return clock.ElapsedMilliseconds.ToString();
+        }
+
+
+         public string ParallelFor(out double nPrimes)
+        {
+            int temp = 0;
+            clock.Restart();
+            Parallel.For(0, N, (i) =>
+            {
+                if (numbers[i].IsPrime())
+                    temp++;
+            });
+            clock.Stop();
+            nPrimes = temp;
+            return clock.ElapsedMilliseconds.ToString();
+        }
+
+
+         public string Linq(out double nPrimes)
+        {
+
+            clock.Restart();
+            var query =
+                from n in numbers
+                where n.IsPrime()
+                select n;
+            nPrimes = query.Count();
+            clock.Stop();
+            return clock.ElapsedMilliseconds.ToString();
+        }
+
+
+         public string PLinq(out double nPrimes)
+        {
+
+            clock.Restart();
+            var query =
+                from n in numbers.AsParallel()
+                where n.IsPrime()
+                select n;
+            nPrimes = query.Count();
+            clock.Stop();
+            return clock.ElapsedMilliseconds.ToString();
+        }
+
+    }
+}
---
+++ tpldemo/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+
+namespace tpldemo
+{
+
+
+   
+
+
+
+
+
+
+    class Program
+    {
+
+
+        protected static int origRow;
+        protected static int origCol;
+
+        protected static void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+
+        protected static void MainMenu()
+        {
+            // Main menu
+            Console.Clear();
+            origRow = Console.CursorTop;
+            origCol = Console.CursorLeft;
+            var horizPos = 22;
+            WriteAt("TPL demo", 33, 2);
+            WriteAt("1 ....... Sequential For", horizPos, 4);
+            WriteAt("2 ....... Parallel For", horizPos, 6);
+            WriteAt("3 ....... Sequential LINQ", horizPos, 8);
+            WriteAt("4 ....... Parallel LINQ", horizPos, 10);
+            WriteAt("0 ....... EXIT PROGRAM", horizPos, 12);
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            var demo = new Demos();
+            Boolean exit = false;
+            ConsoleKeyInfo option;
+            string timeEllapsed=null;
+            double nPrimes;
+            while (!exit)
+            {
+                MainMenu();
+                Console.CursorVisible = false;
+                do
+                {
+                    option = Console.ReadKey(true);
+                } while (option.KeyChar < '0' || option.KeyChar > '5');
+                bool showTime = true;
+
+
+                switch (option.KeyChar)
+                {
+                    case '1':
+                        WriteAt("Calculando......", 22, 16);
+                        timeEllapsed = demo.SequentialFor(out nPrimes);
+                        break;
+                    case '2':
+                        WriteAt("Calculando......", 22, 16);
+                        timeEllapsed = demo.ParallelFor(out nPrimes);
+                        break;
+                    case '3':
+                        WriteAt("Calculando......", 22, 16);
+                        timeEllapsed = demo.Linq(out nPrimes);
+                        break;
+                    case '4':
+                        WriteAt("Calculando......", 22, 16);
+                        timeEllapsed = demo.PLinq(out nPrimes);
+                        break;
+                    case '0':
+                        WriteAt("Calculando......", 22, 16);
+                        exit = true;
+                        showTime = false; 
+                        break;
+                    
+
+                }
+                if (showTime)
+                {
+                    WriteAt($"Ellapsed time {timeEllapsed}ms <Press a key>", 22, 16);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
